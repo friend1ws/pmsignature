dca6157e2747d8fcf6a1da26415ccb7bd6486aa9
Yuichi Shiraishi <friend1ws@gmail.com>
2015-02-10 14:46:24 +0900

    modification

--- R/getSignature.R
+++ R/getSignature.R
@@ -45,12 +45,12 @@ getPMSignature <- function(mutationFeatureData, K, BG = NULL, numInit = 10, tol 
     p0 <- c(convertToTurbo_F(as.vector(F), fdim, K, isBG), convertToTurbo_Q(as.vector(t(Q)), K, sampleNum));
     Y <- list(list(sampleNum, fdim, slot(mutationFeatureData, "featureVectorList"), slot(mutationFeatureData, "countData")), K, isBG, BG);  
     
-    res1 <- turboEM::turboem(par=p0, y=Y, fixptfn=updatePMSParam, objfn=calcPMSLikelihood, method=c("squarem"), pconstr=PMSboundary(Y), control.run = list(convtype = "objfn", tol = 1e-4,  maxiter = 20000));
-    cat(paste("#trial: ", sprintf("%2d", kkk), 
-              "; #iteration: ", sprintf("%4d", as.integer(res1$itr)), 
-              "; time(s): ", sprintf("%4.2f", res1$runtime[3]), 
-              "; loglikelihood: ", sprintf("%.4f", res1$value.objfn), "\n", sep=""
-    ));
+    # res1 <- turboEM::turboem(par=p0, y=Y, fixptfn=updatePMSParam, objfn=calcPMSLikelihood, method=c("squarem"), pconstr=PMSboundary(Y), control.run = list(convtype = "objfn", tol = 1e-4,  maxiter = 20000));
+    # cat(paste("#trial: ", sprintf("%2d", kkk), 
+    #           "; #iteration: ", sprintf("%4d", as.integer(res1$itr)), 
+    #           "; time(s): ", sprintf("%4.2f", res1$runtime[3]), 
+    #           "; loglikelihood: ", sprintf("%.4f", res1$value.objfn), "\n", sep=""
+    # ));
     
     res1 <- mySquareEM(p0, Y, tol = tol, maxIter = maxIter);
     cat(paste("#trial: ", sprintf("%2d", kkk), 
@@ -236,6 +236,7 @@ mySquareEM <- function(p, y, tol = 1e-4, maxIter = 10000) {
     alpha <- max(step.min, min(step.max, alpha));
     p.new <- p + 2 * alpha * q1 + alpha^2 * (q2 - q1);
     
+    newFlag <- 0;
     # This step is done in the original turboEM code...
     # but I cannot understand why this step is necessary....
     if (isTRUE(abs(alpha - 1) > 0.01) ) {
@@ -243,6 +244,7 @@ mySquareEM <- function(p, y, tol = 1e-4, maxIter = 10000) {
       p.new <- upd3[[1]];
       newL <- upd3[[2]];  
       updEvalNum <- updEvalNum + 1;
+      newFlag <- 1;
     } else {
       p.new <- p2;
       newL <- L2;
@@ -261,11 +263,11 @@ mySquareEM <- function(p, y, tol = 1e-4, maxIter = 10000) {
       alpha <- 1
       
       # when p.new is O.K....
-    } else {
+    } else if (newFlag == 1) {
       
-        upd4 <- updatePMSParam(p.new, y);
-        p.new <- upd4[[1]];
-        newL <- upd4[[2]];     
+        # upd4 <- updatePMSParam(p.new, y);
+        # p.new <- upd4[[1]];
+        # newL <- upd4[[2]];     
         
         # when the calculated log-likelihood has some problems
         # or the difference betwen the calculated log-likelihood is large...
@@ -322,142 +324,6 @@ mySquareEM <- function(p, y, tol = 1e-4, maxIter = 10000) {
 }
 
 
-#' A function for estimating parameters using Squared EM algorithm
-#' 
-#' @param p this variable includes the parameters for mutation signatures and membership parameters
-#' @param y this variable includes the information on the mutation features, 
-#' the number of mutation signatures specified and so on
-#' @param tol tolerance for the estimation
-#' (when the difference of log-likelihoods become below this value, stop the estimation)
-#' @param maxIter the maximum number of iteration of estimation
-#' @export
-mySquareEM.bk <- function(p, y, tol = 1e-4, maxIter = 10000) {
-  
-  prevL <- -Inf;
-  step.min <- 1;
-  step.max <- 1;
-  step.max0 <- 1;
-  mstep <- 4;
-  objfn.inc <- 1;
-  updEvalNum <- 0;
-  LEvalNum <- 0;
-  useSquareEM <- 0;
-  iterNum <- 0;
-  convFlag <- FALSE;
-  startTime <- proc.time();
-  
-  newL <- calcPMSLikelihood(p, y);
-  LEvalNum <- LEvalNum + 1;
-  
-  for (iterNum in 1:maxIter) {
-    
-    p1 <- updatePMSParam(p, y);
-    updEvalNum <- updEvalNum + 1;
-    if ( any(is.nan(unlist(p1))) ) {
-      stop("Error in function evaluation");
-    }
-    
-    q1 <- p1 - p;
-    sr2 <- crossprod(q1);
-    
-    p2 <- updatePMSParam(p1, y);
-    updEvalNum <- updEvalNum + 1;
-    if ( any(is.nan(unlist(p2))) ) {
-      stop("Error in function evaluation");
-    }
-    
-    q2 <- p2 - p1
-    sq2 <- sqrt(crossprod(q2))
-    sv2 <- crossprod(q2 - q1)
-    srv <- crossprod(q1, q2 - q1)
-    
-    # alpha <- switch(ctrl$version, -srv/sv2, -sr2/srv, sqrt(sr2/sv2))
-    alpha <- -srv/sv2;
-    alpha <- max(step.min, min(step.max, alpha));
-    p.new <- p + 2 * alpha * q1 + alpha^2 * (q2 - q1);
-    
-    # This step is done in the original turboEM code...
-    # but I cannot understand why this step is necessary....
-    if (isTRUE(abs(alpha - 1) > 0.01) ) {
-      p.new <- updatePMSParam(p.new, y);
-      updEvalNum <- updEvalNum + 1;
-    }
-    
-    # when p.new has some problems...
-    if (any(is.nan(p.new)) | !PMSboundary(y)(p.new) ) {
-      
-      p.new <- p2;
-      newL <- calcPMSLikelihood(p2, y);
-      LEvalNum  <- LEvalNum + 1;
-      
-      # since there was a problem, consider to reduce the amount of step max
-      if (isTRUE(all.equal(alpha, step.max))) {
-        step.max <- max(step.max0, step.max / mstep);
-      }
-      alpha <- 1
-      
-      # when p.new is O.K....
-    } else {
-      
-      newL <- calcPMSLikelihood(p.new, y);
-      LEvalNum  <- LEvalNum + 1;
-      
-      # when the calculated log-likelihood has some problems
-      # or the difference betwen the calculated log-likelihood is large...
-      if (is.nan(newL) | (newL > prevL + objfn.inc)) {
-        
-        p.new <- p2
-        lnew <- calcPMSLikelihood(p2, y);
-        LEvalNum  <- LEvalNum + 1;
-        
-        # since there was a problem, consider to reduce the amount of step max
-        if (alpha == step.max) {
-          step.max <- max(step.max0, step.max / mstep);
-        }
-        alpha <- 1
-        
-      } else {
-        useSquareEM <- useSquareEM + 1;
-      }
-      
-    }
-    
-    if (isTRUE(all.equal(alpha, step.max))) {
-      step.max <- mstep * step.max;
-    }
-    
-    if (step.min < 0 & isTRUE(all.equal(alpha, step.min))) {
-      step.min <- mstep * step.min;
-    }
-    
-    p <- p.new;
-    
-    # for debugging
-    # print(c(updEvalNum, LEvalNum, useSquareEM, step.min, step.max, newL));
-    
-    if (abs(prevL - newL) < tol) {
-      convFlag <- TRUE;
-      break;
-    }
-    
-    if (!is.nan(newL)) {
-      prevL <- newL;
-    }
-    
-  }
-  
-  calcTime <- proc.time() - startTime;
-  
-  return(list(par = p,
-              value.objfn = newL,
-              itr = iterNum,
-              fpeval = updEvalNum,
-              objfeval = LEvalNum,
-              convergence = convFlag,
-              elapsed.time = calcTime[3]))
-  
-}
-
 
 #' A function for updating parameters using EM-algorithm
 #' 
@@ -494,10 +360,8 @@ updatePMSParam <- function(p, y) {
   Q <- t(Q);
   ####################
   # E-step
-  Theta_L <- updateTheta_NormalizedC(as.vector(patternList), as.vector(sparseCount), as.vector(F), as.vector(Q), fdim, K, sampleNum, patternNum, samplePatternNum, isBG, F0);
-  Theta <- Theta_L[-length(Theta_L)];
-  L <- Theta_L[length(Theta_L)];
-  
+  Theta <- updateTheta_NormalizedC(as.vector(patternList), as.vector(sparseCount), as.vector(F), as.vector(Q), fdim, K, sampleNum, patternNum, samplePatternNum, isBG, F0);
+
   dim(Theta) <- c(K, samplePatternNum);
   
   ####################
@@ -510,10 +374,9 @@ updatePMSParam <- function(p, y) {
   dim(Q) <- c(K, sampleNum);
   Q <- t(Q);
   
-  return(list(c(convertToTurbo_F(as.vector(F), fdim, K, isBG), 
+  return(c(convertToTurbo_F(as.vector(F), fdim, K, isBG), 
                 convertToTurbo_Q(as.vector(Q), K, sampleNum)
-                ), 
-              L));
+                ));
   
 }
 
