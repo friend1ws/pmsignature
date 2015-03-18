681dbd5269401f2e94e8dbb10ab4705c724091ca
Yuichi Shiraishi <friend1ws@gmail.com>
2015-03-18 12:58:13 +0900

    modified mutation signature visualization (allowing for dispense with text)

--- R/visualizeSignature.R
+++ R/visualizeSignature.R
@@ -85,7 +85,7 @@ visPMS_ind <- function(vF, numBases, baseCol = NA, trDir, charSize = 1.2) {
     for(w in 1:4) {
       endx <- startx + A[l,w]
       polygon(c(startx, endx, endx, startx), c(0, 0, 1, 1), col = baseCol[w], border=F);
-      if (endx - startx > 1 / 4) {
+      if (endx - startx > 1 / 4 & !is.null(charSize) & charSize > 0) {
         text(0.5 * (endx + startx), 0.5, num2base[w], col="white", cex=charSize)
       }
       startx <- endx;
@@ -100,7 +100,7 @@ visPMS_ind <- function(vF, numBases, baseCol = NA, trDir, charSize = 1.2) {
     for(ww in 1:4) {
       endy <- starty + B[w,ww];
       polygon(c(startx, endx, endx, startx), c(starty, starty, endy, endy), col=baseCol[ww], border=F);
-      if ((endy - starty > 1 / 4) & (endx - startx > 1 / 4)) {
+      if ((endy - starty > 1 / 4) & (endx - startx > 1 / 4) & !is.null(charSize) & charSize > 0) {
         text(0.5 * (endx + startx), 0.5 * (endy + starty), num2base[ww], col="white", cex=charSize)
       }
       starty <- endy;
@@ -142,7 +142,7 @@ visPMS_ind <- function(vF, numBases, baseCol = NA, trDir, charSize = 1.2) {
   starty <- 2;
   endy <- starty + v3[1];
   polygon(c(startx, endx, endx, startx), c(starty, starty, endy, endy), col=baseCol[5], border=F);
-  if (endy - starty > 1 / 8) {
+  if (endy - starty > 1 / 8 & !is.null(charSize) & charSize > 0) {
     text(0.5 * (startx + endx), 0.5 * (starty + endy), "+", col="white", cex=charSize)
   }
   
@@ -151,7 +151,7 @@ visPMS_ind <- function(vF, numBases, baseCol = NA, trDir, charSize = 1.2) {
   starty <- 2;
   endy <- starty + v3[2];
   polygon(c(startx, endx, endx, startx), c(starty, starty, endy, endy), col=baseCol[6], border=F);
-  if (endy - starty > 1 / 8) {
+  if (endy - starty > 1 / 8 & !is.null(charSize) & charSize > 0) {
     text(0.5 * (startx + endx), 0.5 * (starty + endy), "-", col="white", cex=charSize)
   }
   
