3a5d857b5a4f637b8399820e056f1af6c9345753
Yuichi Shiraishi <friend1ws@gmail.com>
2015-06-23 10:46:09 +0900

    add test in case of having background signatures

--- tests/testthat/test_mfv.R
+++ tests/testthat/test_mfv.R
@@ -4,18 +4,35 @@ context("mutation feature vector format reading and estimation")
 test_that("read mutation feature vector format data and estimation (type: indepenent, numBases: 5, trDir: TRUE)", {
   inputFile <- system.file("extdata/Hoang_MFVF.ind.txt", package="pmsignature")
   G <- readMFVFile(inputFile, numBases = 5, trDir = TRUE, type = "independent")
+  BG_prob <- readBGFile(G)
+  
   Param <- getPMSignature(G, K = 3)
   for (k in 1:3) {
     expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 6))
   }
+  
+  Param <- getPMSignature(G, K = 3, BG = BG_prob)
+  for (k in 1:2) {
+    expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 6))
+  }
+  
+  
 })
 
 
 test_that("read mutation feature vector format data and estimation (type: full, numBases: 5, trDir: TRUE)", {
   inputFile <- system.file("extdata/Hoang_MFVF.full.txt", package="pmsignature")
   G <- readMFVFile(inputFile, numBases = 5, trDir = TRUE, type = "full")
+  BG_prob <- readBGFile(G)
+  
   Param <- getPMSignature(G, K = 3)
   for (k in 1:3) {
     expect_equal(sum(getSignatureValue(Param, k)), 1)
   }
+  
+  Param <- getPMSignature(G, K = 3)
+  for (k in 1:2) {
+    expect_equal(sum(getSignatureValue(Param, k)), 1)
+  }
+  
 })
--- tests/testthat/test_mp.R
+++ tests/testthat/test_mp.R
@@ -1,37 +1,73 @@
 context("mutation position format reading and estimation")
 
 test_that("read mutation position format data and estimation (type: indepenent, numBases: 3, trDir: FALSE)", {
+  
   inputFile <- system.file("extdata/Nik_Zainal_2012.mutationPositionFormat.txt", package="pmsignature")
   G <- readMPFile(inputFile, numBases = 3, trDir = FALSE)
+  BG_prob <- readBGFile(G)
+  
   Param <- getPMSignature(G, K = 3)
   for (k in 1:3) {
     expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 3))
   }
+  
+  Param <- getPMSignature(G, K = 3, BG = BG_prob)
+  for (k in 1:2) {
+    expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 3))
+  }
+  
 })
 
 test_that("read mutation position format data and estimation (type: indepenent, numBases: 3, trDir: TRUE)", {
+  
   inputFile <- system.file("extdata/Nik_Zainal_2012.mutationPositionFormat.txt", package="pmsignature")
   G <- readMPFile(inputFile, numBases = 3, trDir = TRUE)
+  BG_prob <- readBGFile(G)
+  
   Param <- getPMSignature(G, K = 3)
   for (k in 1:3) {
     expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 4))
   }
+  
+  Param <- getPMSignature(G, K = 3, BG = BG_prob)
+  for (k in 1:2) {
+    expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 4))
+  }
+  
 })
 
 test_that("read mutation position format data and estimation (type: indepenent, numBases: 5, trDir: FALSE)", {
+  
   inputFile <- system.file("extdata/Nik_Zainal_2012.mutationPositionFormat.txt", package="pmsignature")
   G <- readMPFile(inputFile, numBases = 5, trDir = FALSE)
+  BG_prob <- readBGFile(G)
+  
   Param <- getPMSignature(G, K = 3)
   for (k in 1:3) {
     expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 5))
   }
+  
+  Param <- getPMSignature(G, K = 3, BG = BG_prob)
+  for (k in 1:2) {
+    expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 5))
+  }
+  
 })
 
 test_that("read mutation position format data and estimation (type: indepenent, numBases: 5, trDir: TRUE)", {
+  
   inputFile <- system.file("extdata/Nik_Zainal_2012.mutationPositionFormat.txt", package="pmsignature")
   G <- readMPFile(inputFile, numBases = 5, trDir = TRUE)
+  BG_prob <- readBGFile(G)
+  
   Param <- getPMSignature(G, K = 3)
   for (k in 1:3) {
     expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 6))
   }
+  
+  Param <- getPMSignature(G, K = 3, BG = BG_prob)
+  for (k in 1:2) {
+    expect_equal(rowSums(getSignatureValue(Param, k)), rep(1, 6))
+  }
+  
 })
